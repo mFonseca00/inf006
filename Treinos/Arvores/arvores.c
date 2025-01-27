e0ae71bd77f0219d9818533bd4f3673c68e2743c
Marcus Fonseca <mv070900@gmail.com>
2025-01-27 13:08:43 -0300

    Treinos - Inicio da inserção arvores

--- Treinos/Arvores/arvores.c
+++ Treinos/Arvores/arvores.c
@@ -1,6 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct no{
+  int valor;
+  struct no *dir;
+  struct no *esq;
+}NoArv;
+
+typedef struct {
+  No *raiz;
+  int altura;
+}Arv;
+
+initArv( Arv *arvore){
+  arvore->raiz=NULL;
+  arvore->altura=0;
+}
+
+insertArv(Arv *arvore, int num){
+  NoArv *novo = malloc(sizeof(No));
+  if(novo){
+    novo->valor = num;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    if(arvore->raiz == NULL){
+      arvore->raiz=novo;
+      arvore->altura++;
+    }
+    else{
+      
+    }
+  }
+  else{
+    printf("Erro ao alocar memoria\n");
+}
+
 int main(void){
   return 0;
 }
