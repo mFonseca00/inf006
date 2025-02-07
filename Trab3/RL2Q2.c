586a58ce0d27e020c7cb745b699de5d81558aa59
Marcus Fonseca <mv070900@gmail.com>
2025-02-07 22:25:48 +0000

    Trab 3 - Q2 - função de calculo (necessário teste

--- Trab3/RL2Q2.c
+++ Trab3/RL2Q2.c
@@ -42,7 +42,7 @@ Arvore *criar_arv_vazia(){
     return nova;
 }
 
-int inserir_arv(Arvore *arvore, No *novo){
+void inserir_arv(Arvore *arvore, No *novo){
     if(arvore->raiz==NULL){
         arvore->raiz=novo;
     }
@@ -72,8 +72,6 @@ int inserir_arv(Arvore *arvore, No *novo){
         novo->pai = pai;
         novo->altura = alt;
     }
-
-    return novo->altura;
 }
 
 No *maximo_arv(No *no){ // Busca o nó de maior valor naquela árvore ou sub-árvore
@@ -84,15 +82,24 @@ No *maximo_arv(No *no){ // Busca o nó de maior valor naquela árvore ou sub-ár
     return aux;
 }
 
-int soma_valores_arvore(No *raiz){ // Realiza recursivamente a soma dos valores a direita - somados valores a esquerda e armazena no no->soma
-    int somaDir = 0, somaEsq = 0;
-    if (raiz != NULL){
-        
+int soma_subarvore(No *raiz) { // Realiza a soma dos valores de uma subarvore (esquerda ou direita)
+    if (raiz == NULL) {
+        return 0;
+    }
+    return raiz->valor + soma_subarvore(raiz->esq) + soma_subarvore(raiz->dir);
+}
 
-        somaDir = soma_valores_arvore(raiz->dir);
-        somaEsq = soma_valores_arvore(raiz->esquerda);
+void calcular_soma_valores_arvore(No *raiz) { // Realiza a subtração dos valores da direita pela esquerda em cada nó da arvore
+    if (raiz != NULL) {
+        int somaEsq = soma_subarvore(raiz->esq);
+        printf("No: %d soma subarvore esquerda: %d\t",raiz->valor,somaEsq); // DEBUG
+        int somaDir = soma_subarvore(raiz->dir);
+        printf("No: %d soma subarvore esquerda: %d\t",raiz->valor,somaDir); // DEBUG
+        raiz->soma = somaDir - somaEsq; // Realiza a subtração entre a subarvore atual direita e atual esquerda
+        printf("No: %d resultado entre subarvores: %d",raiz->valor,raiz->soma); // DEBUG
 
-        raiz->soma = somaDir-somaEsq;
+        calcular_soma_valores_arvore(raiz->esq); // Calcula para a esquerda
+        calcular_soma_valores_arvore(raiz->dir); // Calcula para a direita
     }
 }
 
@@ -102,4 +109,64 @@ void liberar_arvore(No *raiz) {
         liberar_arvore(raiz->dir); // Libera subárvore direita
         free(raiz);                 // Libera o nó atual
     }
+}
+
+int main (void){
+    // Ponteiros para os arquivos de entrada e saída
+    FILE *fp_in = fopen("L2Q1.in", "r"); // Abre o arquivo de leitura ("r")
+    FILE *fp_out = fopen("L1Q1.out", "w"); // Abre o arquivo de escrita ("w")
+
+    if (fp_in == NULL || fp_out == NULL) // Tratamento de erro 
+    {
+        printf("File cannot be oppened");
+        return EXIT_FAILURE;
+    }
+
+    char line[MaxCaractersLinha];
+    int valor;
+    char *token;
+    Arvore *arv;
+    int contLinha=0;
+
+    while(fgets(line, sizeof(line), fp_in) != NULL){
+        // Usar strtok para dividir a linha em tokens (números)
+        token = strtok(line, " \n"); // Delimitadores: espaço e nova linha
+        
+        // Pula linha, caso não seja a primeira
+        if(contLinha>0){
+            fprintf(fp_out,"\n");
+            printf("\n"); // DEBUG
+        }
+        contLinha++;
+
+        // Criar árvore
+        arv = criar_arv_vazia();
+
+        while (token != NULL) {
+            if (sscanf(token, "%d", &valor) == 1) {
+                printf("%d ", valor); // DEBUG
+
+                // Inserção dos valores na árvore
+                inserir_arv(arv,criar_no(valor));
+
+            }
+
+            token = strtok(NULL, " \n"); // Obter o próximo token
+        }
+
+        // Calculo da diferença entre as subarvores
+        calcular_soma_valores_arvore(arv->raiz);
+
+        // impressão em string no formato ou direto no arquivo
+        imprimir_dados_arquivo(arv){
+
+        }
+
+        // Excluir árvore
+        liberar_arvore(arv->raiz);
+        free(arv);
+    }
+    fclose(fp_in);
+    fclose(fp_out);
+    return EXIT_SUCCESS;
 }
